e048e22e9fa411c7df33cfd62bf5466cdc727cff
sxc1767732958 <1767732958@qq.com>
2022-03-06 15:58:54 +0800

    1.cpp

---
+++ 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int mian()
+{
+    cout << "hello world " << endl;
+    return 0;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "1" << endl;
+    }
+
+
+
+    
+    return 0 ;
+} 
